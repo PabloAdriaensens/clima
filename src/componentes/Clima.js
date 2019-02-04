fda6803b6fdf440d9230af178d1ca5a76bade3d0
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-04 21:50:31 +0100

    Showing the Clima we select in the form

--- src/componentes/Clima.js
+++ src/componentes/Clima.js
@@ -7,12 +7,29 @@ class Clima extends Component {
         // Obtener los datos de la consulta
 
         const {name, weather, main} = this.props.resultado;
+        if (!name || !weather || !main) return null;
 
-        if(!name || !weather || !main) return null;
+        const kelvin = 273.15;
 
-        return(
+        const urlIcono = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
+
+        const alt = `clima de ${name}`;
+
+        return (
             <div className="row">
-                {name}
+                <div className="resultado col s12 m8 l6 offset-m2 offset-l3">
+                    <div className="card-panel light-blue align-center">
+                        <span className="white-text">
+                            <h2>Resultado Clima de: {name}</h2>
+                            <p className="temperatura">
+                                Actual: {(main.temp - kelvin).toFixed(2)} &deg;C
+                                <img src={urlIcono} alt={alt}/>
+                            </p>
+                            <p> Max. {main.temp_max - kelvin} &deg;C</p>
+                            <p> Min. {main.temp_min - kelvin} &deg;C</p>
+                        </span>
+                    </div>
+                </div>
             </div>
         )
 
@@ -20,7 +37,6 @@ class Clima extends Component {
 
     render() {
 
-        console.log(this.props.resultado);
         return (
             <div className="container">
                 {this.mostrarResultado()}
